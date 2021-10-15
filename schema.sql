e40f6c2c4d1a488fe4ae4757b22e22db3a61d3d9
ale-river <alejandra.rvrs@gmail.com>
2021-10-14 23:09:44 -0500

    schema_sql

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE employees(
      PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
+CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -35,14 +35,14 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE managers (
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
